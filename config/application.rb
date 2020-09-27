d9ed8d082beeed2e80f27080192a5e53f5648f4f
Leonardo Viada <leo@revodigital.it>
2020-09-27 16:31:12 +0200

    postgres migration

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-
-    config.force_ssl = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,20 @@
 
 ActiveRecord::Schema.define(version: 2020_09_27_082322) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.text "title"
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.text "commenter"
     t.text "body"
-    t.integer "article_id", null: false
+    t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
